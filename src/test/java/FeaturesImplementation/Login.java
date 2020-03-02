71966716b05944d600ca83bf3b92f9d86735a0ef
Radoslav Topalov <topalov.radoslav@gmail.com>
2020-03-02 16:26:53 +0200

    Login xpath update

--- src/test/java/FeaturesImplementation/Login.java
+++ src/test/java/FeaturesImplementation/Login.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.By;
 public class Login {
     @When("Login with Username: {string} and Password: {string}")
     public void login_with_Username_and_Password(String username, String password) {
-        BrowserSetup.driver.findElement(By.xpath("//input[@placeholder='Phone, email or username']")).sendKeys(username);
-        BrowserSetup.driver.findElement(By.xpath("//input[@class='js-password-field']")).sendKeys(password);
-        BrowserSetup.driver.findElement(By.xpath("//button[@type='submit']")).click();
+        BrowserSetup.driver.findElement(By.xpath("//main[@role=\"main\"]//input[@name=\"session[username_or_email]\"]")).sendKeys(username);
+        BrowserSetup.driver.findElement(By.xpath("//main[@role=\"main\"]//input[@name=\"session[password]\"]")).sendKeys(password);
+        BrowserSetup.driver.findElement(By.xpath("//main[@role=\"main\"]//div[@data-testid=\"LoginForm_Login_Button\"]")).click();
         //BrowserSetup.driver.navigate().refresh();
     }
 }
--- target/test-classes/Login.feature
+++ target/test-classes/Login.feature
@@ -5,7 +5,6 @@ Feature: Login page
     When Login with Username: "gocigey967@tmailcloud.com" and Password: "Otawa@1"
     And Post new Tweet with text "New Tweet!"
     And Delete tweet
-   # Then Tweet is deleted
     Then Verify Tweet is deleted with "Your Tweet was deleted" alert message
 
 
